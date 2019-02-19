d1a124094fe257844a7382ab2051468fc3d949c9
Abby Gottlich <abby.gottlich@gmail.com>
2019-02-19 17:31:22 -0600

    fetch hooked up

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://agottlich:abby123@ds335275.mlab.com:35275/checkpoint-one", { useNewUrlParser: true });
 const bodyParser = require("body-parser");
 const app = express();
+const fetch = require('node-fetch');
 
 const messageRoutes = require("./Routes/MessageRoutes");
 const taskRoutes = require("./Routes/TaskRoutes");
@@ -18,4 +19,10 @@ app.listen(3001, (err) => {
         return console.log("Error", err);
     }
     console.log("Web server is now listening for messages", err);
+});
+
+app.get('/foxes', function (req, res) {
+    fetch('https://randomfox.ca/floof/')
+        .then(res => res.json())
+        .then(body => res.json(body));
 });
